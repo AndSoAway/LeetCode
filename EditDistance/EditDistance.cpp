d829d30d811e257b28f131067a2b8536c9798904
Qinger <xyqhello@gmail.com>
2015-06-17 21:14:40 +0800

    modified

--- EditDistance/EditDistance.cpp
+++ EditDistance/EditDistance.cpp
@@ -12,25 +12,26 @@ public:
 		for(int i = 0; i < len1 + 1; i++) {
 			dis[i] = new int[len2 + 1];
 		}
-		for(int i = 0; i < len2; i++) {
-			dis[0][i] = len1;
+		for(int i = 0; i < len2 + 1; i++) {
+			dis[0][i] = len2;
 		}
-		for(int i = 0; i < len1; i++) {
-			dis[i][0] = len2;
+		for(int i = 0; i < len1 + 1; i++) {
+			dis[i][0] = len1;
 		}
 		dis[0][0] = 0;
 		for(int i = 1; i <= len1; i++) {
 			for(int j = 1; j <= len2; j++) {
-				int diff = word1[i] != word2[j];
+				int diff = word1[i - 1] != word2[j - 1];
+				printf("i is %d, j is %d, diff is %d\n", i, j, diff);
 				dis[i][j] = min(min(1 + dis[i - 1][j], 1 + dis[i][j - 1]), diff + dis[i - 1][j - 1]);
 			}
 		}
+		int res = dis[len1][len2];
 		for(int i = 0; i <= len1; i++) {
 			delete[] dis[i];
 		}
 		delete dis;
-		int res = dis[len1][len2];
-		return dis[len1][len2];
+		return res;
 	} 
 
 private:
@@ -40,8 +41,10 @@ private:
 };
 
 int main() {
-	string word1("sunny");
-	string word2("snowy");
+//	string word1("dinitrophenylhydrazine");
+//	string word2("acetylphenylhydrazine");
+	string word1("a");
+	string word2("b");
 	Solution solution;
 	printf("EditDistance: %d\n", solution.minDistance(word1, word2));
 	return 0;
